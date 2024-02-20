b51f89932e5ed21be9a5dcc2b836ca306f45e524
Edgar A. Chan <edgarandrei1791@outlook.com>
2024-02-20 15:34:39 -0600

    Working on sidebar features

--- index.php
+++ index.php
@@ -66,4 +66,3 @@
         <!-- Footer -->
 
         <?php include_once('templates/footer.php'); ?>
-
--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,16 +1,38 @@
+<?php
+
+    include_once('scripts/db.php');
+
+    if(isset($_POST['submit'])){
+        $search = $_POST['search'];
+
+        $query = "SELECT * FROM posts WHERE tags LIKE '%$search%'";
+        $result = mysqli_query($connection, $query);
+
+        if($result){
+            while($row=mysqli_fetch_assoc($result)){
+                echo '<pre>';
+                print_r($row);
+                echo '<pre>';
+            }
+        }
+    }
+?>
+
 <div class="col-md-4">
 
 <!-- Blog Search Well -->
 <div class="well">
     <h4>Blog Search</h4>
-    <div class="input-group">
-        <input type="text" class="form-control">
-        <span class="input-group-btn">
-            <button class="btn btn-default" type="button">
-                <span class="glyphicon glyphicon-search"></span>
-        </button>
-        </span>
-    </div>
+    <form action="" method="POST">
+        <div class="input-group">
+            <input type="text" name="search" class="form-control">
+            <span class="input-group-btn">
+                <button class="btn btn-default" type="submit" name="submit">
+                    <span class="glyphicon glyphicon-search"></span>
+            </button>
+            </span>
+        </div>
+    </form>
     <!-- /.input-group -->
 </div>
 
@@ -20,18 +42,22 @@
     <div class="row">
         <div class="col-lg-6">
             <ul class="list-unstyled">
-                <li><a href="#">Category Name</a>
-                </li>
-                <li><a href="#">Category Name</a>
-                </li>
-                <li><a href="#">Category Name</a>
-                </li>
-                <li><a href="#">Category Name</a>
-                </li>
+                <?php 
+                
+                    $query = "SELECT * FROM categories";
+                    $result = mysqli_query($connection, $query);
+
+                    if($result){
+                        while($row=mysqli_fetch_assoc($result)){
+                            echo "<li><a href='#'>{$row['title']}</a></li>";
+                        }
+                    }
+
+                ?>
             </ul>
         </div>
         <!-- /.col-lg-6 -->
-        <div class="col-lg-6">
+        <!-- <div class="col-lg-6">
             <ul class="list-unstyled">
                 <li><a href="#">Category Name</a>
                 </li>
@@ -42,7 +68,7 @@
                 <li><a href="#">Category Name</a>
                 </li>
             </ul>
-        </div>
+        </div> -->
         <!-- /.col-lg-6 -->
     </div>
     <!-- /.row -->
@@ -50,8 +76,18 @@
 
 <!-- Side Widget Well -->
 <div class="well">
-    <h4>Side Widget Well</h4>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
+    <h4>Recent Blog Posts</h4>
+    <?php 
+        $query = "SELECT title, content FROM posts";
+        $result = mysqli_query($connection, $query);
+        if($result){
+            while($row=mysqli_fetch_assoc($result)){
+                echo "<h5>{$row['title']}</h5>".
+                     "<p>{$row['content']}</p>";
+            }
+        }
+    ?>
+    
 </div>
 
 </div>
