defe9a4d6bdc525ce3ca5b571dc2a91e6536bb21
Edgar A. Chan <edgarandrei1791@outlook.com>
2024-02-20 12:19:07 -0600

    Showing posts from the database & adding the images to the images folder

--- index.php
+++ index.php
@@ -21,19 +21,36 @@
                     <small>Secondary Text</small>
                 </h1>
 
-                <!-- First Blog Post -->
-                <h2>
-                    <a href="#">Blog Post Title</a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
-                <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-                <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                <?php
+                    include_once('scripts/db.php'); 
+                    $query = "SELECT * FROM posts";
+
+                    $result = mysqli_query($connection, $query);
+
+                    if($result){
+                        while($row=mysqli_fetch_assoc($result)){
+
+                            $title = $row['title'];
+                            $author = $row['author'];
+                            $date = $row['date'];
+                            $image = $row['image'];
+                            $content = $row['content'];
+
+                            echo '<h2>'.
+                                    '<a href="#">'.$title.'</a>'.
+                                '</h2>'.
+                                '<p class="lead">'.
+                                    'by <a href="index.php">'.$author.'</a>'.
+                                '</p>'.
+                                '<p><span class="glyphicon glyphicon-time"></span> Posted on '.$date.'</p>'.
+                                '<hr>'.
+                                '<img class="img-responsive" src="images/'.$image.'" alt="">'.
+                                '<hr>'.
+                                '<p>'.$content.'</p>'.
+                                '<a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>';
+                        }
+                    }                
+                ?>                
 
                 <!-- <hr>        -->
             </div>
