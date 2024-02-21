1780626220f4de787d73602f9b5163e6b233cf7a
Edgar A. Chan <edgarandrei1791@outlook.com>
2024-02-21 09:33:22 -0600

    showing categories from the database on a bootstrap table

--- admin/categories.php
+++ admin/categories.php
@@ -33,6 +33,28 @@
                                 </div>
                             </form>
                         </div>
+                        <div class="col-md-6">
+                            <table class="table table-bordered table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>Id</th>
+                                        <th>Title</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php 
+                                        $query = "SELECT * FROM categories";
+                                        $result = mysqli_query($connection, $query);
+
+                                        if($result){
+                                            while($row=mysqli_fetch_assoc($result)){
+                                                echo "<tr><td>{$row['category_id']}</td><td>{$row['title']}</td></tr>";
+                                            }
+                                        }
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
                 <!-- /.row -->
