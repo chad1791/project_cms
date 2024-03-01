1fad3fdbf6cf28a136754f8bb280946278b47474
Edgar A. Chan <edgarandrei1791@outlook.com>
2024-03-01 10:24:50 -0600

    Creation of comments page and comments script - showing all comments from the database

---
+++ admin/comments.php
@@ -0,0 +1,60 @@
+<?php 
+    include_once('templates/header.php'); 
+    include_once('scripts/comments.php');
+    //deletePost();    
+?>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <?php include_once('templates/navbar.php'); ?>
+
+        <div id="page-wrapper">
+            <div class="container-fluid">
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            All Coments
+                            <small>View Comments</small>
+                        </h1>
+                        <ol class="breadcrumb">
+                            <li>
+                                <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
+                            </li>
+                            <li class="active">
+                                <i class="fa fa-file"></i> Comments 
+                            </li>
+                        </ol>
+                        <div class="col-md-12">
+                            <table class="table table-bordered table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>Id</th>
+                                        <th>Author</th>
+                                        <th>Comment</th>
+                                        <th>Email</th>
+                                        <th>Status</th>
+                                        <th>In Response To</th>
+                                        <th>Date</th>
+                                        <th>Options</th> <!-- Approve, Unapprove, delete -->
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php 
+                                        getAllComments();
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+        </div>
+        <!-- /#page-wrapper -->
+    </div>
+    <!-- /#wrapper -->
+
+<?php include_once('templates/footer.php'); ?>
---
+++ admin/scripts/comments.php
@@ -0,0 +1,34 @@
+<?php
+    function getAllComments(){
+        global $connection;
+        $query = "SELECT * FROM comments";
+        $result = mysqli_query($connection, $query);
+
+        if($result){
+            while($row=mysqli_fetch_assoc($result)){
+                echo "<tr>".
+                "<td>{$row['comment_id']}</td>".
+                "<td>{$row['author_id']}</td>".
+                "<td>{$row['content']}</td>".
+                "<td>{$row['author_email']}</td>".
+                "<td>{$row['status']}</td>".
+                "<td>{$row['post_id']}</td>".
+                "<td>{$row['date']}</td>".
+                "<td>".
+                    "<a href='comments.php?approve={$row['post_id']}'>".
+                        "<button class='btn btn-primary' data-toggle='tooltip' data-placement='top' title='Approve'><i class='fa fa-check-square-o'></i></button>".
+                    "</a>".
+                    "&nbsp;&nbsp;".
+                    "<a href='comments.php?unapprove={$row['post_id']}'>".
+                    "<button class='btn btn-primary' data-toggle='tooltip' data-placement='top' title='Unapprove'><i class='fa fa-square-o'></i></button>".
+                    "</a>".
+                    "&nbsp;&nbsp;".
+                    "<a href='comments.php?delete={$row['post_id']}'>".
+                        "<button class='btn btn-danger'><i class='fa fa-trash'></i></button>".
+                    "</a>".
+                "</td>".
+              "</tr>";
+            }
+        }
+    }
+?>
